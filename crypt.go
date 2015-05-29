bf0fc179afc11462df87ec32529b6a6d19382f19
Inderpreet Singh <inder@wastedcode.com>
2015-05-28 20:43:19 -0700

    Initial commit

---
+++ crypt.go
@@ -0,0 +1,118 @@
+package crypt
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/hex"
+    "errors"
+    "io"
+)
+
+type Crypt struct {
+    UnencryptedData []byte
+    CipherData []byte
+    Key []byte
+}
+
+func NewCryptFromPlainText(plainText, key string) (*Crypt) {
+    return NewCryptFromUnencryptedData([]byte(plainText), key)
+}
+
+func NewCryptFromUnencryptedData(data []byte, key string) (*Crypt) {
+    ValidateCryptKey(key)
+    crypt := Crypt {
+        UnencryptedData: data,
+        Key: []byte(key)}
+    return &crypt
+}
+
+func NewCryptFromHexCipherText(cipherText, key string) (*Crypt) {
+    return NewCryptFromCipherData(DecodeHexString(cipherText), key)
+}
+
+func NewCryptFromCipherData(cipherData []byte, key string) (*Crypt) {
+    ValidateCryptKey(key)
+    crypt := Crypt {
+        CipherData: cipherData,
+        Key: []byte(key)}
+    return &crypt
+}
+
+func (crypt *Crypt) Encrypt() ([]byte, error) {
+    block, err := aes.NewCipher(crypt.Key)
+    if (err != nil) {
+        return nil, err
+    }
+
+    crypt.CipherData = make([]byte, aes.BlockSize + len(crypt.UnencryptedData))
+
+    // Generate initialization vector
+    // This will generate a random vector
+    iv := crypt.CipherData[:aes.BlockSize]
+    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+        return nil, err
+    }
+
+    // Use a stream cipher to encrypt the data
+    stream := cipher.NewCFBEncrypter(block, iv)
+    stream.XORKeyStream(crypt.CipherData[aes.BlockSize:], crypt.UnencryptedData)
+
+    return crypt.CipherData, nil
+}
+
+func (crypt *Crypt) EncryptToString() (string, error) {
+    data, err := crypt.Encrypt()
+    if (err != nil) {
+        return "", err
+    }
+    return hex.EncodeToString(data), nil
+}
+
+func (crypt *Crypt) Decrypt() ([]byte, error) {
+    block, err := aes.NewCipher(crypt.Key)
+    if (err != nil) {
+        return nil, err
+    }
+
+    if (len(crypt.CipherData) < aes.BlockSize) {
+        return nil, errors.New("Invalid cipher text")
+    }
+
+    iv := crypt.CipherData[:aes.BlockSize]
+    cipherText := crypt.CipherData[aes.BlockSize:]
+    crypt.UnencryptedData = make([]byte, len(cipherText))
+    stream := cipher.NewCFBDecrypter(block, iv)
+
+    stream.XORKeyStream(crypt.UnencryptedData, cipherText)
+    return crypt.UnencryptedData, nil
+}
+
+func (crypt *Crypt) DecryptToString() (string, error) {
+    data, err := crypt.Decrypt()
+    if (err != nil) {
+        return "", nil
+    }
+    return string(data), nil
+}
+
+// Decodes the string from hex to a byte array
+// Panics if the input string is not hex encoded
+func DecodeHexString(text string) ([]byte) {
+    textBytes, err := hex.DecodeString(text)
+    if (err != nil) {
+        panic("The given string: " + text + " is not a valid hex string")
+    }
+    return textBytes
+}
+
+func ValidateCryptKey(key string) {
+    keyLength := len(key)
+    validSizes := []int {16, 24, 32}
+    for _, size := range validSizes {
+        if (keyLength == size) {
+            return
+        }
+    }
+    panic("The given key is of invalid size")
+}
---
+++ crypt_test.go
@@ -0,0 +1,189 @@
+package crypt
+
+import (
+    "encoding/hex"
+    "testing"
+)
+
+var cryptKeys = []string {
+    "1234567890123456",
+    "123456789012345678901234",
+    "12345678901234567890123456789012",
+}
+
+func TestDecodeHexString(t *testing.T) {
+    testStr := "testing string"
+    hexStr := hex.EncodeToString([]byte(testStr))
+    if (string(DecodeHexString(hexStr)) != testStr) {
+        t.Error("Should have decoded to " + testStr)
+    }
+
+    testStr = ""
+    hexStr = hex.EncodeToString([]byte(testStr))
+    if (string(DecodeHexString(hexStr)) != testStr) {
+        t.Error("Could not decode empty string")
+    }
+}
+
+func TestDecodeHexStringInvalidInput(t *testing.T) {
+    testStr := "invalid hex"
+    defer func() {
+        r := recover()
+        if (r == nil) {
+            t.Error("Failed to panic with invalid hex")
+        }
+    }()
+    DecodeHexString(testStr)
+}
+
+func TestValidateCryptKey(t *testing.T) {
+    defer func() {
+        if (recover() != nil) {
+            t.Error("Failed to accept a valid crypt key")
+        }
+    }()
+    for _, key := range cryptKeys {
+        ValidateCryptKey(key)
+    }
+}
+
+func TestValidateCryptKeyWithInvalidKey(t *testing.T) {
+    defer func() {
+        if (recover() == nil) {
+            t.Error("Failed to panic when an invalid key was given")
+        }
+    }()
+
+    ValidateCryptKey("invalidkey")
+}
+
+func TestNewCryptFromPlainText(t *testing.T) {
+    text := "plain text"
+    key := cryptKeys[0]
+    crypt := NewCryptFromPlainText(text, key)
+    if (string(crypt.UnencryptedData) != text) {
+        t.Errorf("Unable to initialize crypt with plain text '%s'", text)
+    }
+
+    if (string(crypt.Key) != key) {
+        t.Errorf("Unable to initialize crypt with key '%s'", key)
+    }
+}
+
+func TestNewCryptFromUnencryptedData(t *testing.T) {
+    text := "plain text"
+    data := []byte(text)
+    key := cryptKeys[0]
+    crypt := NewCryptFromUnencryptedData(data, key)
+    if (string(crypt.UnencryptedData) != text) {
+        t.Errorf("Unable to initialize crypt with plain text '%s'", text)
+    }
+
+    if (string(crypt.Key) != key) {
+        t.Errorf("Unable to initialize crypt with key '%s'", key)
+    }
+}
+
+func TestNewCryptFromHexCipherText(t *testing.T) {
+    textStr := "teststring"
+    hexStr := hex.EncodeToString([]byte(textStr))
+    key := cryptKeys[1]
+    crypt := NewCryptFromHexCipherText(hexStr, key)
+    if (string(crypt.CipherData) != textStr) {
+        t.Errorf("Unable to initialize crypt with hex data '%s'", hexStr)
+    }
+
+    if (string(crypt.Key) != key) {
+        t.Errorf("Unable to initialize crypt with key '%s'", key)
+    }
+}
+
+func TestNewCryptFromCipherData(t *testing.T) {
+    textStr := "teststring"
+    key := cryptKeys[1]
+    crypt := NewCryptFromCipherData([]byte(textStr), key)
+    if (string(crypt.CipherData) != textStr) {
+        t.Errorf("Unable to initialize crypt with hex data '%s'", textStr)
+    }
+
+    if (string(crypt.Key) != key) {
+        t.Errorf("Unable to initialize crypt with key '%s'", key)
+    }
+}
+
+func TestEncryptDecryptString(t *testing.T) {
+    stringToEncrypt := "some string"
+    key := cryptKeys[0]
+    crypt := NewCryptFromPlainText(stringToEncrypt, key)
+    encrypted, err := crypt.Encrypt()
+
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (hex.EncodeToString(crypt.CipherData) != hex.EncodeToString(encrypted)) {
+        t.Error("Did not set the cipher data after encryption")
+    }
+
+    decrypted, err := crypt.DecryptToString()
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (decrypted != stringToEncrypt) {
+        t.Errorf("Unable to encrypt/decrypt string: '%s'", stringToEncrypt)
+    }
+
+    crypt = NewCryptFromCipherData(encrypted, key)
+    decrypted, err = crypt.DecryptToString()
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (decrypted != stringToEncrypt) {
+        t.Errorf("Unable to encrypt/decrypt string: '%s'", stringToEncrypt)
+    }
+}
+
+func TestEncryptDecryptThroughHex(t *testing.T) {
+    stringToEncrypt := "some string"
+    key := cryptKeys[0]
+    crypt := NewCryptFromPlainText(stringToEncrypt, key)
+    encrypted, err := crypt.EncryptToString()
+
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (hex.EncodeToString(crypt.CipherData) != encrypted) {
+        t.Error("Did not set the cipher data after encryption")
+    }
+
+    decrypted, err := crypt.DecryptToString()
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (decrypted != stringToEncrypt) {
+        t.Errorf("Unable to encrypt/decrypt string: '%s'", stringToEncrypt)
+    }
+
+    crypt = NewCryptFromHexCipherText(encrypted, key)
+    decrypted, err = crypt.DecryptToString()
+    if (err != nil) {
+        t.Error("Unable to encrypt string")
+    }
+
+    if (decrypted != stringToEncrypt) {
+        t.Errorf("Unable to encrypt/decrypt string: '%s'", stringToEncrypt)
+    }
+}
+
+func TestDecryptWithInvalidData(t *testing.T) {
+    key := cryptKeys[0]
+    crypt := NewCryptFromHexCipherText(hex.EncodeToString([]byte("lesthnbsize")), key)
+    _, err := crypt.Decrypt()
+    if (err == nil) {
+        t.Error("Did not fail with invalid text for decryption")
+    }
+}
